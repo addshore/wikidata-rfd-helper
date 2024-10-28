c4a7dd70610f63369b82aabffc362245b3c8a381
addshore <addshorewiki@gmail.com>
2024-10-28 13:59:30 +0000

    Add ref stuff

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         Highlight Non-Notable Properties on Wikidata (Fork)
+// @name         Highlights non-notable elements of a Wikidata Item (Fork)
 // @namespace    https://www.wikidata.org/
 // @version      1.0
-// @description  Highlights non-notable properties on Wikidata
-// @author       User:Shisma
+// @description  Highlights non-notable elements of a Wikidata Item
+// @author       User:Shisma,User:Addshore
 // @match        https://www.wikidata.org/wiki/*
 // @grant        GM_xmlhttpRequest
 // @run-at       document-end
@@ -89,7 +89,49 @@
         }
     }
 
+    // TODO if thee are 0 terms (labels descriptions aliases), draw a red outline around the terms section
+    // TODO if there are 0 statements, draw a red outline around the statements section
+    // TODO if a statement has 0 references, draw a red outline around the references sections
+
+    function checkReferences() {
+        const elements = document.querySelectorAll('.wikibase-referenceview');
+        const siteIds = sitelinkIds();
+        const P_referenceUrl = 'P854';
+        const P_basedOnHeuristic = 'P887';
+        // Loop over each reference
+        elements.forEach(async (element) => {
+            let propertyValueMap = {};
+            // Find each wikibase-snaklistview in the referenceview (which is an indivudal snak)
+            const snakLists = element.querySelectorAll('.wikibase-snaklistview');
+            snakLists.forEach((snakList) => {
+                // Find the property ID
+                const propertyId = snakList.querySelector('.wikibase-snakview-property').childNodes[0].href.match(/Property:(P\d+)/)[1];
+                // Find the value of the property, which is in the text content of wikibase-snakview-value
+                const propertyValue = snakList.querySelector('.wikibase-snakview-value').textContent;
+                propertyValueMap[propertyId] = propertyValue;
+            });
+            // If a reference is based on a heuristic, it's not a reliable source, mark as red
+            if (propertyValueMap[P_basedOnHeuristic]) {
+                element.style.outline = '2px solid red';
+            }
+            // If a reference is a URL, and the domain is the the bad list, mark as red
+            const badList = [
+                'amazon.com',
+                'amazon.co.uk',
+            ]
+            if (propertyValueMap[P_referenceUrl]) {
+                const url = propertyValueMap[P_referenceUrl];
+                const domain = new URL(url).hostname;
+                if (badList.includes(domain)) {
+                    element.style.outline = '2px solid red';
+                }
+            }
+            // TODO check if the reference says imported from Wikipedia (P143), but no such sitelink exists, then mark as red
+        });
+    }
+
     // Run the checks once the page has loaded
     window.addEventListener('load', checkNonNotableProperties);
     window.addEventListener('load', checkSitelinks);
+    window.addEventListener('load', checkReferences);
 })();
