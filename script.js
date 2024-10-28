fe7725b9b72cbab189504ffd2fea8c9e06e9ddc7
addshore <addshorewiki@gmail.com>
2024-10-28 14:01:25 +0000

    Start suplementing Wikidata:Requests_for_deletions too

--- script.js
+++ script.js
@@ -33,27 +33,94 @@
         });
     }
 
-    function addLinkToTitle() {
-        // Add a link to the end of the inner HTML of wikibase-title, called "RFD view", which runs checkStatementGroups and checkSitelinks
-        const title = document.querySelector('.wikibase-title');
-        const link = document.createElement('a');
-        link.href = '#';
-        link.textContent = 'RFD view';
-        link.onclick = function() {
-            runAllChecks();
-            return false;
-        };
-        title.appendChild(link);
-    }
+    function onLoad() {
+        // Add a link to the top of item pages
+        if (window.location.pathname.match(/\/wiki\/Q\d+/)) {
+            // Add a link to the end of the inner HTML of wikibase-title, called "RFD view", which runs checkStatementGroups and checkSitelinks
+            const title = document.querySelector('.wikibase-title');
+            const link = document.createElement('a');
+            link.href = '#';
+            link.textContent = 'RFD view';
+            link.onclick = function() {
+                runAllItemChecks();
+                return false;
+            };
+            title.appendChild(link);
+        }
+        // If we are on wiki/Wikidata:Requests_for_deletions, add stuff
+        if (window.location.pathname.match(/\/wiki\/Wikidata:Requests_for_deletions/)) {
+            // Find each mw-heading2 section
+            const headings = document.querySelectorAll('.mw-heading2');
+            headings.forEach(async (heading) => {
+                // If we add content, it will be to a div at the end of the heading content :)
+                const div = document.createElement('div');
+                heading.appendChild(div);
+                const rfdHelper = document.createElement('span');
+                rfdHelper.textContent = 'RFD Helper: ';
+                div.appendChild(rfdHelper);
+                // We want to find the next ol tag after "heading", if it is a group,
+                // Or find the next p tag after "heading" if it is a single entry
+                const nextElement = heading.nextElementSibling;
+                // Within that next element, find all li tags, or use the p tag as the only li
+                const rfdItems = nextElement.tagName === 'OL' ? nextElement.querySelectorAll('li') : [nextElement];
+                rfdItems.forEach(async (rfdItem) => {
+                    if (!rfdItem.querySelector('a')) {
+                        console.log('No link found in RFD item');
+                        console.log(rfdItem);
+                        return;
+                    }
+                    // find the attribute title of the a link, this is the item id
+                    const itemId = rfdItem.querySelector('a').title;
+                    // skip if it is not a Q item
+                    if (!itemId.match(/^Q\d+$/)) {
+                        console.log('Not a Q item, skipping item:', itemId);
+                        return;
+                    }
+                    try{
+                        // Lookup the first revision of the item from the API, and add the author and timestamp to the content of the page
+                        // https://www.wikidata.org/w/api.php?action=query&prop=revisions&titles=Q123&rvlimit=1&rvprop=timestamp|user&format=json
+                        let data = await fetchJSON(`https://www.wikidata.org/w/api.php?action=query&prop=revisions&titles=${itemId}&rvlimit=1&rvprop=timestamp|user&format=json`)
+                        console.log(data);
+                        let pageId = Object.keys(data.query.pages)[0];
+                        let firstRevisionData = data.query.pages[pageId].revisions[0];
+                        let author = firstRevisionData.user;
+                        let timestamp = firstRevisionData.timestamp;
+                        let timeSinceCreation = new Date() - new Date(timestamp);
+                        let daysSinceCreation = Math.floor(timeSinceCreation / (1000 * 60 * 60 * 24));
+                        let timeSinceCreationString = `${daysSinceCreation} days ago`;
+                        if (daysSinceCreation === 1) {
+                            timeSinceCreationString = '1 day ago';
+                        }
+                        if (daysSinceCreation === 0) {
+                            timeSinceCreationString = 'today';
+                        }
+                        let authorElement = document.createElement('span');
+                        authorElement.innerHTML = `Created by <a href="https://www.wikidata.org/wiki/User:${author}">${author}</a> ${timeSinceCreationString}`;
+                        div.appendChild(authorElement);
 
-    function loadOnRFDRefer() {
-        // If the URL contains "Special:EntityData", run checkStatementGroups and checkSitelinks
-        if (document.referrer === 'https://www.wikidata.org/wiki/Wikidata:Requests_for_deletions') {
-            runAllChecks();
+                        // If the user is NOT an IP address, then add a link to send them a message on their talk page
+                        if (!author.match(/\d+\.\d+\.\d+\.\d+/)) {
+                            let talkLink = document.createElement('a');
+                            talkLink.href = `https://www.wikidata.org/wiki/User_talk:${author}`;
+                            talkLink.textContent = ' (talk)';
+                            authorElement.appendChild(talkLink);
+                        }
+                    } catch (error) {
+                        console.error(`Error fetching data and suplementing info for ${itemId}:`, error);
+                    }
+                });
+            })
         }
     }
 
-    function runAllChecks() {
+    function runAllItemChecksOnRFDRefer() {
+        // If the URL contains "Special:EntityData", run checkStatementGroups and checkSitelinks
+        if (document.referrer === 'https://www.wikidata.org/wiki/Wikidata:Requests_for_deletions') {
+            runAllItemChecks();
+        }
+    }
+
+    function runAllItemChecks() {
         checkSitelinks();
         checkStatementGroups();
     }
@@ -92,6 +159,11 @@
 
         // Each property used for statements creates 1 statement group view
         const statementGroups = document.querySelectorAll('.wikibase-statementgroupview[id]');
+        // if there are no statemens, highlight wikibase-statements in red
+        if (statementGroups.length === 0) {
+            const element = document.querySelector('.wikibase-statements');
+            element.style.outline = '2px solid red';
+        }
         statementGroups.forEach(async (statementGroup) => {
             const statementGroupID = statementGroup.id;
             console.log(statementGroupID);
@@ -162,6 +234,6 @@
         });
     }
 
-    window.addEventListener('load', addLinkToTitle);
-    window.addEventListener('load', loadOnRFDRefer);
+    window.addEventListener('load', onLoad);
+    window.addEventListener('load', runAllItemChecksOnRFDRefer);
 })();
